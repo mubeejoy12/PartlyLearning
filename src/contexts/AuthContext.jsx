c003bbffdf967a7b0cce86ac12a0dfd1da4c845a
mubeejoy12 <83483923+mubeejoy12@users.noreply.github.com>
2025-03-04 23:49:53 +0100

    use reducer issntead of use state to handle complex usestate

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -1,22 +1,45 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useReducer, useState } from "react";
+
+const initialState = {
+  isAuthenticated: false,
+  userName: "",
+  // login: () => {},
+  // logout: () => {},
+};
+
+// Reducer function to manage state
+const authReducer = (state, action) => {
+  switch (action.type) {
+    case "LOGIN":
+      return { isAuthenticated: true, username: action.payload };
+    case "LOGOUT":
+      return { isAuthenticated: false, username: "" };
+    default:
+      return state;
+  }
+};
 
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-  const [userName, setUserName] = useState("");
+  const [state, dispatch] = useReducer(authReducer, initialState);
 
-  const login = (name) => {
-    setIsAuthenticated(true);
-    setUserName(name);
-  };
-  const logout = () => {
-    setIsAuthenticated(false);
-    setUserName("");
-  };
+  const login = (username) => dispatch({ type: "LOGIN", payload: username });
+  const logout = () => dispatch({ type: "LOGOUT" });
+  //   const [isAuthenticated, setIsAuthenticated] = useState(false);
+  //   const [userName, setUserName] = useState("");
+
+  //   const login = (name) => {
+  //     setIsAuthenticated(true);
+  //     setUserName(name);
+  //   };
+  //   const logout = () => {
+  //     setIsAuthenticated(false);
+  //     setUserName("");
+  //   };
 
   return (
-    <AuthContext.Provider value={{ isAuthenticated, login, logout, userName }}>
+    <AuthContext.Provider value={{ ...state, login, logout }}>
       {children}
     </AuthContext.Provider>
   );
